84cc2b90e2e83da0c83e8dc314c35eb96d0d133f
Michael Nnaji <mnnaji3@uwo.ca>
2020-01-03 22:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector){
+  var  selected = document.querySelector(selector)
+  return(selected)
+}
+
+function nestedTarget(){
+  var output = document.querySelector('#nested .target')
+  return(output)
+}
+
+function deepestChild(){
+  var output1 = document.querySelector('#grand-node div div div div')
+  return(output1)
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = (parseInt(lis[i].innerHTML,10) + n).toString())
+  }
+  for (let i = 0; i < lis.length; i++) {
+   console.log(lis[i].innerHTML)
+ }
+}
